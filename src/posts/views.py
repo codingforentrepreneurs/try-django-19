4ea20b6d10f18845fefdf284d7cd190bfeac1f72
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-12-14 18:31:02 -0800

    16 - QuerySet Basics

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Post
 
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
@@ -14,11 +14,12 @@ def post_detail(request): #retrieve
 	return render(request, "index.html", context)
 
 def post_list(request): #list items
-
+	queryset = Post.objects.all()
 	context = {
+		"object_list": queryset, 
 		"title": "List"
 	}
-	
+
 	# if request.user.is_authenticated():
 	# 	context = {
 	# 		"title": "My User List"
