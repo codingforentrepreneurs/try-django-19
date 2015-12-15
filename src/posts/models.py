10e740127a6c8f9f72ed767aca3550bc7b39bb5c
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-12-14 17:10:59 -0800

    6 - First App & Model

---
+++ src/posts/models.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# MVC MODEL VIEW CONTROLLER
+
+class Post(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
+
+	def __str__(self):
+		return self.title
