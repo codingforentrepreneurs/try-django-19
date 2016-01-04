c05ef6fdb86c42f14dda608bd2f732ed5a81e454
CFE <cfe@Justins-iMac-2.local>
2016-01-04 13:24:27 -0800

    18 - Dyanmic Url Routing & Patterns

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = [
 	url(r'^$', post_list),
     url(r'^create/$', post_create),
-    url(r'^detail/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail),
     url(r'^update/$', post_update),
     url(r'^delete/$', post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
--- src/posts/views.py
+++ src/posts/views.py
@@ -7,9 +7,9 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request): #retrieve
+def post_detail(request, id=None): #retrieve
 	#instance = Post.objects.get(id=1)
-	instance = get_object_or_404(Post, id=4)
+	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
 		"instance": instance,
