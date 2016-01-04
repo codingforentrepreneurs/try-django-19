42d795ec9878d121da3011233c3a53792fb1efea
CFE <cfe@Justins-iMac-2.local>
2016-01-04 13:35:26 -0800

    19 - URL Links & Get Absolute URL

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,18 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
 class Post(models.Model):
-	title = models.CharField(max_length=120)
-	content = models.TextField()
-	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"id": self.id})
--- src/trydjango19/urls.py
+++ src/trydjango19/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls")),
+    url(r'^posts/', include("posts.urls", namespace='posts')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
