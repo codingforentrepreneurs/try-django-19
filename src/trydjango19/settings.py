adcc96bb9e014a948ed86d1ae43243deb6565b08
CFE <hello@teamcfe.com>
2016-01-04 15:53:11 -0800

    25 - Setup Static Files - CSS - Javascript - Images in Django

--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -120,3 +120,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/trydjango19/urls.py
+++ src/trydjango19/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     url(r'^posts/', include("posts.urls", namespace='posts')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
