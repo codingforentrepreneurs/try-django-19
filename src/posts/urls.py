754e4d86fb3682be3cd77fd99592cdb16b186f8e
CFE <hello@teamcfe.com>
2016-01-04 15:22:17 -0800

    23 - Delete View

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -10,10 +10,10 @@ from .views import (
 	)
 
 urlpatterns = [
-	url(r'^$', post_list),
+	url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    url(r'^delete/$', post_delete),
+    url(r'^(?P<id>\d+)/delete/$', post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -55,5 +55,8 @@ def post_update(request, id=None):
 
 
 
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully deleted")
+	return redirect("posts:list")
