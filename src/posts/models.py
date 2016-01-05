87773b59e475a40861b306d350ae5292b349e633
CFE <hello@teamcfe.com>
2016-01-04 19:18:28 -0800

    33 - Associate User to Post with a Foreign Key

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
@@ -16,6 +16,7 @@ def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, 
--- src/posts/views.py
+++ src/posts/views.py
@@ -11,9 +11,11 @@ from .models import Post
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
+		
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		instance.user = request.user
 		instance.save()
 		# message success
 		messages.success(request, "Successfully Created")
