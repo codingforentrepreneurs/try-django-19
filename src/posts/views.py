711e883a61a6067e9d3f533c43bcca9dcf72e606
CFE <hello@teamcfe.com>
2016-01-04 18:58:10 -0800

    30 - Social Share Links

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,5 @@
+from urllib import quote_plus
+
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
@@ -21,9 +23,11 @@ def post_create(request):
 
 def post_detail(request, slug=None):
 	instance = get_object_or_404(Post, slug=slug)
+	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
 		"instance": instance,
+		"share_string": share_string,
 	}
 	return render(request, "post_detail.html", context)
 
