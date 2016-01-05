23d29efefefd3124b9beca80e1307570c7239741
CFE <hello@teamcfe.com>
2016-01-04 16:13:38 -0800

    27 - Pagination by QuerySet

--- src/posts/models.py
+++ src/posts/models.py
@@ -20,3 +20,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={"id": self.id})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -27,12 +28,29 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
+
 	context = {
 		"object_list": queryset, 
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var
 	}
 	return render(request, "post_list.html", context)
+
+
+
 
 
 def post_update(request, id=None):
