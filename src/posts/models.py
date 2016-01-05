f3038795c8145476262258b0b6db6f11b9dd7bca
CFE <hello@teamcfe.com>
2016-01-05 14:30:28 -0800

    36 - Model Managers & Handling Drafts

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,10 +4,20 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils import timezone
 
 from django.utils.text import slugify
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
+
+
+#Post.objects.all()
+#Post.objects.create(user=user, title="Some time")
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        # Post.objects.all() = super(PostManager, self).all()
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 
 def upload_location(instance, filename):
@@ -31,6 +41,8 @@ class Post(models.Model):
     publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .forms import PostForm
 from .models import Post
@@ -27,6 +28,9 @@ def post_create(request):
 
 def post_detail(request, slug=None):
 	instance = get_object_or_404(Post, slug=slug)
+	if instance.publish > timezone.now().date() or instance.draft:
+		if not request.user.is_staff or not request.user.is_superuser:
+			raise Http404
 	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
@@ -36,7 +40,10 @@ def post_detail(request, slug=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	today = timezone.now().date()
+	queryset_list = Post.objects.active() #.order_by("-timestamp")
+	if request.user.is_staff or request.user.is_superuser:
+		queryset_list = Post.objects.all()
 	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
 	page_request_var = "page"
 	page = request.GET.get(page_request_var)
@@ -53,7 +60,8 @@ def post_list(request):
 	context = {
 		"object_list": queryset, 
 		"title": "List",
-		"page_request_var": page_request_var
+		"page_request_var": page_request_var,
+		"today": today,
 	}
 	return render(request, "post_list.html", context)
 
