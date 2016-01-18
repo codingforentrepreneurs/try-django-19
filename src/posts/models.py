cf9b0617d06da3b61cd942ec57df4687ad5be18a
Justin Mitchel <jmitchel3@gmail.com>
2016-01-17 16:10:44 -0800

    Update image upload_to location to account for new model instances

--- src/posts/models.py
+++ src/posts/models.py
@@ -23,7 +23,16 @@ class PostManager(models.Manager):
 def upload_location(instance, filename):
     #filebase, extension = filename.split(".")
     #return "%s/%s.%s" %(instance.id, instance.id, extension)
-    return "%s/%s" %(instance.id, filename)
+    PostModel = instance.__class__
+    new_id = PostModel.objects.order_by("id").last().id + 1
+    """
+    instance.__class__ gets the model Post. We must use this method because the model is defined below.
+    Then create a queryset ordered by the "id"s of each object, 
+    Then we get the last object in the queryset with `.last()`
+    Which will give us the most recently created Model instance
+    We add 1 to it, so we get what should be the same id as the the post we are creating.
+    """
+    return "%s/%s" %(new_id, filename)
 
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
