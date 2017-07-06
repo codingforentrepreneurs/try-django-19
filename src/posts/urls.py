1a813cd9cfeca75eb4227a96d1e6f5dd807d7ea1
Coding For Entrepreneurs <codingforentrepreneurs@gmail.com>
2017-07-06 14:20:21 -0700

    Django Code Review #3

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,12 +7,14 @@ from .views import (
 	post_detail,
 	post_update,
 	post_delete,
+	PostDetailView
 	)
 
 urlpatterns = [
 	url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
-    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    #url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/$', PostDetailView.as_view(), name='detail'), #Django Code Review #3 on joincfe.com/youtube/
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
